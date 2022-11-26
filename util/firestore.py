cadff14f6f2e8068e48acf10ace07411dc48ca18
danand <danand1101@gmail.com>
2022-11-26 20:28:44 +0100

    added dotenv

--- util/firestore.py
+++ util/firestore.py
@@ -1,17 +1,20 @@
+from firebase_admin import firestore
+from firebase_admin import credentials
+import firebase_admin
 import json
 import os
 from base64 import b64decode
+from dotenv import load_dotenv, find_dotenv
 
-import firebase_admin
-from firebase_admin import credentials
-from firebase_admin import firestore
+load_dotenv(find_dotenv())
 
 
 def get_firestore_db():
     firebase_config = os.getenv("FIREBASE")
+    print(b64decode(firebase_config))
     firebase_dict = json.loads(b64decode(firebase_config))
 
     cred = credentials.Certificate(firebase_dict)
     firebase_admin.initialize_app(cred)
-
+    print('ufff')
     return firestore.client()
